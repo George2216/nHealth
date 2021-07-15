de79c31ee54bb4feda03e45dee00c8a927c102da
georgeChurikov <georgechurikov@gmail.com>
2021-07-15 12:00:22 +0300

    Initial Commit

---
+++ AppointmentProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppointmentProject
+//
+//  Created by George on 15.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
